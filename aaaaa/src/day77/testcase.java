1cf5bdf400990a77b966e705e0f217ce9560ee8b
vamsi <vamsikoppula56@gmail.com>
2023-10-10 18:39:27 +0530

    koppulawer

---
+++ aaaaa/src/day77/testcase.java
@@ -0,0 +1,15 @@
+package day77;
+
+import org.testng.annotations.Test;
+
+public class testcase {
+  @Test
+  public void openapp() {
+	  System.out.println("opening the application");
+	  
+  }
+  @Test
+  public void closeapp(){
+	  System.out.println("close the application");
+  }
+}
